bb2a3714a54f938de3cb5adcba40c8a4e7335f7b
ImGib <59463222+ImGib@users.noreply.github.com>
2023-02-21 13:22:58 +0700

    push-jsp-marketing

--- QuizSystem/src/main/java/Controller/Expert/ManageSubjectController.java
+++ QuizSystem/src/main/java/Controller/Expert/ManageSubjectController.java
@@ -2,29 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Controller;
+package Controller.Expert;
 
-import dao.impl.AccountDao;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
-import model.Account;
-import service.impl.AccountService;
 
-/**
- *
- * @author Lenovo
- */
-@WebServlet(urlPatterns = {"/home", "/hello"})
-public class HiContoller extends HttpServlet{
+@WebServlet(urlPatterns = {"/expert-subject-management"})
+public class ManageSubjectController extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("views/user/home.jsp").forward(req, resp);
-
+        super.doGet(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+    
 }
